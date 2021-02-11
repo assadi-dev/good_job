658d8ce16b2b97570a7323db0b25fee5fd5f3a3c
unknown <mars974@live.fr>
2021-02-11 12:11:59 +0100

    Update Accueil.js

--- assets/components/Accueil/Accueil.js
+++ assets/components/Accueil/Accueil.js
@@ -2,14 +2,14 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import Filtres from './Filtres';
 import ListOffres from './ListOffres';
-import { CONTRAT } from '../store/action'
+
 
 
 
 
 const Accueil = () => {
     
-    console.log(CONTRAT);
+
    
     return (
   
